3f8189b9088e8fc46a4a71b2309c159387093555
Alessandra Romero <24320222+alexgromero@users.noreply.github.com>
2021-08-04 01:34:34 -0400

    updated TicketRepository.java
    removed testing methods that won’t be used

--- src/model/TicketRepository.java
+++ src/model/TicketRepository.java
@@ -10,63 +10,53 @@ import java.util.HashMap;
  * Value: the ticket object 
  */
 public class TicketRepository {
-	private final int ATTRIBUTES = 7;
-	
+
 	public HashMap<Integer, Ticket> ticketRepo;
-	
+
 	public TicketRepository() {
 		ticketRepo = new HashMap<Integer, Ticket>();
 	}
-	
+
 	public void addTicket(Integer id, Ticket ticket) {
 		ticketRepo.put(id, ticket);
 	}
-	
+
 	public int size() {
 		return ticketRepo.size();
 	}
-	
+
 	public ArrayList<Integer> keySet() {
 		ArrayList<Integer> keys = new ArrayList<Integer>(ticketRepo.keySet());
 		return keys;
 	}
+
 	public boolean isTicketInRepo(int id) {
 		if (ticketRepo.containsKey(id)) {
 			return true;
 		}
 		return false;
 	}
-	
-	public ArrayList<String> allTicketInfo(int id) {
-		ArrayList<String> allInfo = new ArrayList<>();
 
-		allInfo.add(String.valueOf(ticketRepo.get(id).getId()));
-		allInfo.add(ticketRepo.get(id).getStatus());
-		allInfo.add(ticketRepo.get(id).getSubject());
-		
-		return allInfo;
+	public ArrayList<String> allTicketInfo(int id) {
+		ArrayList<String> info = new ArrayList<>();
+
+		info.add(String.valueOf(ticketRepo.get(id).getId()));
+		info.add(ticketRepo.get(id).getStatus());
+		info.add(ticketRepo.get(id).getSubject());
+
+		return info;
 	}
-	
+
 	public ArrayList<String> singleTicketInfo(int id) {
 		ArrayList<String> info = new ArrayList<>();
-		
+
 		info.add(String.valueOf(ticketRepo.get(id).getRequesterId()));
 		info.add(ticketRepo.get(id).getStatus());
 		info.add(ticketRepo.get(id).getCreatedAt());
 		info.add(ticketRepo.get(id).getUpdatedAt());
 		info.add(ticketRepo.get(id).getSubject());
 		info.add(ticketRepo.get(id).getDescription());
-		
+
 		return info;
 	}
-	
-	public void printTicketInfo(int id) {
-		System.out.println(Arrays.asList(singleTicketInfo(id)));
-	}
-	
-	// TODO - REMOVE THIS METHOD
-	public void print() {
-		System.out.println(ticketRepo.entrySet());
-	}
-	
 }
