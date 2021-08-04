d1ded0f811448629f3e686444b15a67a024314d3
Alessandra Romero <24320222+alexgromero@users.noreply.github.com>
2021-08-04 01:15:57 -0400

    remove comment from Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 * This is the main driver class which will run the CLI Ticket Viewer application
 	 */
 	public static void main(String[] args) {
-		TicketRepository model = new TicketRepository(); // maybe NOT?
+		TicketRepository model = new TicketRepository();
 		TicketViewerInterface screen = new TicketViewerInterface();
 		TicketViewerController controller = new TicketViewerController(model, screen);
 
